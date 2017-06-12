f48117cdb96dc8a07a5aab9ae003b0e37a5dc7df
byung-u <iam.byungwoo@gmail.com>
2017-06-12 11:03:57 +0900

    ADD for flatpages edit

--- namingworks/naming/admin.py
+++ namingworks/naming/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.flatpages.admin import FlatPageAdmin
+from django.contrib.flatpages.models import FlatPage
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django_summernote.widgets import SummernoteWidget
 
-# Register your models here.
+class SummernoteWidgetWithCustomToolbar(SummernoteWidget):
+    def template_contexts(self):
+        contexts = super(SummernoteWidgetWithCustomToolbar, self).template_contexts()
+        contexts['width'] = '960px'
+        return contexts
+
+class FlatPageAdmin(FlatPageAdmin):
+    formfield_overrides = {models.TextField: {'widget': SummernoteWidgetWithCustomToolbar}}
+
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, FlatPageAdmin)
+
