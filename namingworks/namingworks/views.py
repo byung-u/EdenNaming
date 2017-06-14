49d3f05739aa932ce3346cb377e143ec62b39403
byung-u <iam.byungwoo@gmail.com>
2017-06-14 20:32:25 +0900

    ADD title

--- namingworks/namingworks/views.py
+++ namingworks/namingworks/views.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+from django.utils.translation import ugettext as _
 
 
 def index(request):
     return render(request, 'index.html', {
+        'title': _('미래작명당'),
         'index': True,
     })
