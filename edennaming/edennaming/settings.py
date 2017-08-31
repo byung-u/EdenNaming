fccbadbad880f67673a53c3122a3cf41b9698a2a
byung-u <iam.byungwoo@gmail.com>
2017-08-31 11:48:18 +0900

    Fix path

--- edennaming/edennaming/settings.py
+++ edennaming/edennaming/settings.py
@@ -119,7 +119,7 @@ SITE_ID = 2
 #STATICFILES_DIRS = (
 #    os.path.join(BASE_DIR, 'static'),
 #)
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
